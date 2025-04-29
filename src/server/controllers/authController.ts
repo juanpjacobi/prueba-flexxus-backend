26ad60d346bde80b14fed84436e036de820188d9
= <=>
2025-04-29 20:55:36 -0300

    jwt, helmet y cors added

---
+++ src/server/controllers/authController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.JWT_SECRET || "CAMBIA_ESTE_SECRETO";
+
+export const login = (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (username === "admin" && password === "1234") {
+    const user = { id: 1, username };
+    const token = jwt.sign({ user }, SECRET, { expiresIn: "1h" });
+    res.json({ token });
+  }
+  res.status(401).json({ message: "Credenciales inválidas" });
+};
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,25 @@
 import express from "express";
 import { PORT } from "../config";
 import apiRouter from "./routes";  
+import helmet from "helmet";
+import cors from 'cors';
+import { authenticateToken } from "./middlware/auth";
+import authRouter from "./routes/auth.routes";
 
 const app = express();
+
+app.use(helmet());
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    methods: ['GET','POST','PATCH','DELETE','OPTIONS'],
+    credentials: true,
+  }));
+
 app.use(express.json());
-app.use("/api", apiRouter);
+//rutas publicas
+app.use('/api/auth', authRouter);
+
+app.use('/api', authenticateToken, apiRouter);
 
 export default app;
