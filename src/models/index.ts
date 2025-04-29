afe9e4da036602e12edbb6691334c8cc8eb1471c
= <=>
2025-04-29 00:04:33 -0300

    feat: modelo Departamento y migración inicial con FK a Localidades

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,5 @@
 export { Article } from "./article/Article";
 export { Puesto } from "./puesto/Puesto";
+export { Localidad } from "./localidad/Localidad";
+export { Departamento } from "./departamento/Departamento"
+
