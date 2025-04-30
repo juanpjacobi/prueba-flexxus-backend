40c33dc4058e4a91e2db9cf302f5f4d11e9351b7
= <=>
2025-04-30 04:30:52 -0300

    updated

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,41 +1,41 @@
 import express from "express";
 import { PORT } from "../config";
-import apiRouter from "./routes";  
+import apiRouter from "./routes";
 import helmet from "helmet";
-import cors from 'cors';
+import cors from "cors";
 import { authenticateToken } from "./middlware/auth";
 import authRouter from "./routes/auth.routes";
 
-
-
 const app = express();
-
-
 
 app.use(helmet());
 
-const allowedLocal = 'http://localhost:5173';
+const allowedLocal = "http://localhost:5173";
 
-app.use(cors({
-  origin: (incomingOrigin, callback) => {
-    if (
-      !incomingOrigin ||
-      incomingOrigin === allowedLocal ||
-      incomingOrigin.endsWith('.netlify.app')
-    ) {
-      return callback(null, true);
-    }
-    console.warn(`🚫 CORS bloqueo para Origin=${incomingOrigin}`);
-    return callback(new Error(`Origin ${incomingOrigin} no permitido por CORS`));
-  },
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: (incomingOrigin, callback) => {
+      if (
+        !incomingOrigin ||
+        incomingOrigin === allowedLocal ||
+        incomingOrigin.endsWith(".netlify.app")
+      ) {
+        return callback(null, true);
+      }
+      console.warn(`🚫 CORS bloqueo para Origin=${incomingOrigin}`);
+      return callback(
+        new Error(`Origin ${incomingOrigin} no permitido por CORS`)
+      );
+    },
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Authorization"],
+  })
+);
 app.use(express.json());
 //rutas publicas
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 
-app.use('/api', authenticateToken, apiRouter);
-
-
+app.use("/api", authenticateToken, apiRouter);
 
 export default app;
