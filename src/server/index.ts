fdb497bc7a7693c8c7a4e4503da905951c9e5386
= <=>
2025-04-30 04:19:29 -0300

    cors updated

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,6 +6,11 @@ import cors from 'cors';
 import { authenticateToken } from "./middlware/auth";
 import authRouter from "./routes/auth.routes";
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://prueba-flexxus-frontend.netlify.app'
+];
+
 const app = express();
 
 
@@ -13,10 +18,16 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-    methods: ['GET','POST','PATCH','DELETE','OPTIONS'],
-    credentials: true,
-  }));
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS: Origen ${origin} no permitido`));
+    }
+  },
+  methods: ['GET','POST','PATCH','DELETE','OPTIONS'],
+  credentials: true,
+}));
 
 app.use(express.json());
 //rutas publicas
