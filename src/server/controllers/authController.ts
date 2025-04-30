76b177440a15091ecd8428a859442b16ec7b3503
= <=>
2025-04-30 01:40:33 -0300

    controller updated

--- src/server/controllers/authController.ts
+++ src/server/controllers/authController.ts
@@ -3,13 +3,14 @@ import jwt from "jsonwebtoken";
 
 const SECRET = process.env.JWT_SECRET || "CAMBIA_ESTE_SECRETO";
 
-export const login = (req: Request, res: Response) => {
+export const login = (req: Request, res: Response, ) => {
   const { username, password } = req.body;
   if (username === "admin" && password === "1234") {
     const user = { id: 1, username };
     const token = jwt.sign({ user }, SECRET, { expiresIn: "1h" });
-    return res.json({ token, user });
+     res.json({ token, user });
+     return;
 
   }
-  return res.status(401).json({ message: "Credenciales inválidas" });
+  res.status(401).json({ message: "Credenciales inválidas" });
 };
